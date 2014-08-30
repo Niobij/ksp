1b44f71411b2fe232ba1cdb8fce81f609528030f
ZOG <zdesbulbac9i@gmail.com>
2014-08-30 01:21:05 -0700

    saves 29.08.14

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,38 @@
 /**
  * Created with IntelliJ IDEA.
- * User: Mangos
+ * User: ZOG
  * Date: 8/24/14
  * Time: 6:25 PM
- * To change this template use File | Settings | File Templates.
  */
 public class Main {
 
 	//weight
 	private static final double W_USEFUL			= 0.1 + 0.84;
 
-	private static final double W_S3_TOTAL			= W_USEFUL + 0.05 + 4 * 0.005 + 3 * 0.05 + 3 * 0.125 + 6 * 0.05 + (0.5625) + 3 * (1.125) + (2.25) + 1.25;
-	private static final double W_S3_DRY			= W_S3_TOTAL - 0.49 - 3 * 1 - 2;
+	private static final double W_S3_TOTAL			= W_USEFUL + 0.5 + 0.12 + 2 * 0.0175 + 3 * 0.05 + 3 * 0.05 + 3 * 0.05 + 4 * (1.125) + 1.25;
+	private static final double W_S3_DRY			= W_S3_TOTAL - 4 * 1;
 
-	private static final double W_S2_TOTAL			= W_S3_TOTAL + 0.05 + (0.5625) + (1.125) + 2 * (2.25) + 1.25;
-	private static final double W_S2_DRY			= W_S2_TOTAL - 0.49 - 1 - 2 * 2;
+	private static final double W_S2_TOTAL			= W_S3_TOTAL + 0.05 + (0.5625) + (4.5) + 1.25;
+	private static final double W_S2_DRY			= W_S2_TOTAL - 0.49 - 4;
 
-	private static final double W_S1_TOTAL			= W_S2_TOTAL + 0.05 + 2 * 0.05 + 18 * 0.05 + 4 * (4.5) + 2 * (18) + 2 * 1.25 + 3;
-	private static final double W_S1_DRY			= W_S1_TOTAL - 4 * 4 - 2 * 16;
+	private static final double W_S1_TOTAL			= W_S2_TOTAL + 0.05 + 0.1 + 4 * 0.02 + (4.5) + 2 * (18) + 3;
+	private static final double W_S1_DRY			= W_S1_TOTAL - 4 - 2 * 16;
 
 
 	public static final void main(final String[] _args) {
-		System.out.println(W_S3_TOTAL);
-		System.out.println(W_S3_DRY);
-		System.out.println(W_S3_TOTAL / W_S3_DRY);
+		System.out.println(W_S1_TOTAL);
+		System.out.println(W_S1_DRY);
+		System.out.println(W_S1_TOTAL / W_S1_DRY);
 
 //		final double[] thrusts = { 650000, 250000, 250000 };
 //		final double[] isps = { 350, 230, 230 };
 //
 //		System.out.println("Average Isp = " + Calculations.calcAverageIsp(thrusts, isps));
 
-		final double isp = 390;
+		final double isp = 350;
 		final double we = Calculations.calcWe(isp);
 
-		System.out.println("Znum = " + Calculations.calcZnum(3200, we));
+		System.out.println("Znum = " + Calculations.calcZnum(3300, we));
 //		System.out.println("deltaV = " + Calculations.calcDeltaV(we, 8.71, 3.71));
 //		System.out.println("TWR = " + Calculations.calcTWR(0, 0, 0));
 	}
